cee12eccd59fdf7b9ebd4d49da8a487ad5b938eb
Oscar Wankaew <oscar.wankaew@gmail.com>
2022-06-28 11:34:58 -0500

    commiting student model with full crud actions and student requests.http

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "/students" => "students#index"
+  get "/students/:id" => "students#show"
+  post "/students" => "students#create"
+  patch "/students/:id" => "students#update"
+  delete "/students/:id" => "students#destroy"
 end
