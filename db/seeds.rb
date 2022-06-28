f7d22dd72439ec46fae1191757f884f955002d71
Kevin Zhong <kvnzhong@gmail.com>
2022-06-28 13:23:02 -0700

    Prepare for push.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-require 'faker'
+require "faker"
 
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
@@ -9,5 +9,11 @@ require 'faker'
 #   Character.create(name: "Luke", movie: movies.first)
 
 5.times do
-  student = Student.create(first_name: "#{Faker::Name.first_name}", last_name: "#{Faker::Name.last_name}", email: "#{Faker::Internet.email}", phone_number: "#{Faker::PhoneNumber.phone_number}", short_bio: {Faker::Quotes::Shakespeare}, linkedin_url:, twitter_handle:)
+  first_name = "#{Faker::Name.first_name}"
+  last_name = "#{Faker::Name.last_name}"
+  email = first_name + last_name + "@test.com"
+  linkedin = "linkedin.com/" + first_name + last_name
+  github = "github.com/" + first_name + last_name
+
+  student = Student.create(first_name: first_name, last_name: last_name, email: email, phone_number: "#{Faker::PhoneNumber.phone_number}", short_bio: "#{Faker::Quote.famous_last_words}", linkedin_url: linkedin, twitter_handle: "#{Faker::Twitter.user.name}", personal_website: )
 end
