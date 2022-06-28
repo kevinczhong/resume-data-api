f8da6ee4ecd06604f9bd0a3ed2603778a56a6083
Kevin Zhong <kvnzhong@gmail.com>
2022-06-28 09:26:21 -0700

    Added skeleton for user controller, user/session routes; implemented User model.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+20.times do
+  Student.create(first_name:, last_name:, email:, phone_number:, short_bio:, linkedin_url:, twitter_handle:)
+end
