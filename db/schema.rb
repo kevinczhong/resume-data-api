734b55754a3eaf1d72d2781b7d65915027e5c352
Oscar Wankaew <oscar.wankaew@gmail.com>
2022-06-28 14:05:03 -0500

    added student_id to capstone model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_28_184911) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_28_190306) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_28_184911) do
     t.string "screenshot"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "student_id"
   end
 
   create_table "educations", force: :cascade do |t|
