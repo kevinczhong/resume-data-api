7a0f7c915dff8c2a092e3fbaff32d55320fd4c31
Julian Anaya <juliananaya531@gmail.com>
2022-06-28 09:39:04 -0700

    cruded it out

---
+++ app/controllers/experiences_controller.rb
@@ -0,0 +1,36 @@
+class ExperiencesController < ApplicationController
+  def index
+    experiences = Experience.all
+    render json: experiences.as_json
+  end
+  def show
+    experience = Experience.find(params["id"])
+    render json: experience.as_json
+  end
+  def create
+    experience = Experience.new(
+      start_date: params[:start_date],
+      end_date: params[:end_date],
+      job_title: params[:job_title],
+      company_name: params[:company_name],
+      details: params[:details],
+    )
+    experience.save
+    render json: experience.as_json
+  end
+  def update
+    experience = Experience.find_by(id: params[:id])
+    experience.start_date = params[:start_date] || experience.start_date
+    experience.end_date = params[:end_date] || experience.end_date
+    experience.job_title = params[:job_title] || experience.job_title
+    experience.company_name = params[:company_name] || experience.company_name
+    experience.details = params[:details] || experience.details
+    experience.save
+    render json: experience.as_json
+  end
+  def destroy
+    experience = Experience.find_by((id: params[:id]))
+    experience.destroy
+    render json: {message: "Experience deleted"}
+  end
+end
