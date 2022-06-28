86c68967b82ca3366b83e903cd6f4311fd44a3c6
Pedro Chang <pedrochang91@gmail.com>
2022-06-28 10:43:03 -0700

    CRUD for education

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_28_162312) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_28_174125) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "educations", force: :cascade do |t|
+    t.date "start_date"
+    t.date "end_date"
+    t.string "degree"
+    t.string "university_name"
+    t.text "details"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "experiences", force: :cascade do |t|
     t.string "start_date"
