4458f0aa7811b4ba7db8efe21890937c16b57491
Kevin Zhong <kvnzhong@gmail.com>
2022-06-28 10:04:51 -0700

    Resolving more merge conflicts, not yet complete.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
   post "/users" => "users#create"
   post "/sessions" => "sessions#create"
   get "/experiences" => "experiences#index"
