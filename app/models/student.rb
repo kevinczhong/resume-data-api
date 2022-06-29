7cdbb0d6a11b0aa38ceb6055ac50b01246293684
Kevin Zhong <kvnzhong@gmail.com>
2022-06-28 18:44:49 -0700

    Added population methods for Skills, StudentSkills to Seeds file, completed association for join table.

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,6 +2,7 @@ class Student < ApplicationRecord
   belongs_to :user
   has_many :experiences
   has_many :educations
-  has_many :skills
+  has_many :student_skills
+  has_many :skills, through: :student_skills
   has_many :capstones
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,14 +8,52 @@ require "faker"
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-5.times do
+# first_name = "#{Faker::Name.first_name}"
+# p first_name
+
+n = 5
+skills = ["Ruby", "Rails", "HTML", "CSS", "Javascript"]
+
+for skill in skills
+  skill_name = skill
+  Skill.create(
+    skill_name: skill_name,
+  )
+end
+
+n.times do
   first_name = "#{Faker::Name.first_name}"
   last_name = "#{Faker::Name.last_name}"
-  email = first_name + last_name + "@test.com"
+  email = first_name.downcase + last_name.downcase + "@test.com"
   linkedin = "linkedin.com/" + first_name + last_name
   github = "github.com/" + first_name + last_name
   personal_website = first_name.downcase + last_name.downcase + "/github.io"
-  online_resume = 
+  online_resume = first_name.downcase + last_name.downcase + "/resumes.com"
 
-  student = Student.create(first_name: first_name, last_name: last_name, email: email, phone_number: "#{Faker::PhoneNumber.phone_number}", short_bio: "#{Faker::Quote.famous_last_words}", linkedin_url: linkedin, twitter_handle: "#{Faker::Twitter.user.name}", personal_website: personal_website, online_resume: )
+  user = User.create(name: first_name, email: email, password_digest: "password")
+
+  student = Student.create(
+    first_name: first_name,
+    last_name: last_name,
+    email: email,
+    phone_number: "#{Faker::PhoneNumber.phone_number}",
+    short_bio: "#{Faker::Quote.famous_last_words}",
+    linkedin: linkedin,
+    twitter_handle: "#{Faker::Twitter.screen_name}",
+    personal_website: personal_website,
+    online_resume: online_resume,
+    github: github,
+    photo: "placeholder.jpg",
+    user_id: user.id,
+  )
+
+  no_skills = rand(1..5)
+  index = 1
+  no_skills.times do
+    StudentSkill.create(
+      student_id: student.id,
+      skill_id: index,
+    )
+    index += 1
+  end
 end
