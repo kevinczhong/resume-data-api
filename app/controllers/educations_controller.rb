f235a0256cd235dc3ce1634b35a4ae155b565051
Oscar Wankaew <oscar.wankaew@gmail.com>
2022-06-30 11:54:47 -0500

    added student_id to capstone, education and experience models

--- app/controllers/educations_controller.rb
+++ app/controllers/educations_controller.rb
@@ -16,6 +16,7 @@ class EducationsController < ApplicationController
       degree: params[:degree],
       university_name: params[:university_name],
       details: params[:details],
+      student_id: params[:student_id],
     )
     education.save
     render json: education.as_json
@@ -23,6 +24,7 @@ class EducationsController < ApplicationController
 
   def update
     education = Education.find_by(id: params[:id])
+    education.student_id = params[:student_id] || education.student_id
     education.start_date = params[:start_date] || education.start_date
     education.end_date = params[:end_date] || education.end_date
     education.degree = params[:degree] || education.degree
